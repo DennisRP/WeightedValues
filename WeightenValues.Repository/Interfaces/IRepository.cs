fcde1067b7ac134232d7977aff7f5c50a8db9970
Dennis <dennisrp@gmail.com>
2014-09-26 01:20:34 +0200

    Data access layer

---
+++ WeightenValues.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeightenValues.Repository.Interfaces
+{
+    // Define interface for easier replacement of storage
+    public interface IRepository
+    {
+        Dictionary<int, string> GetEducations { get; }
+        Dictionary<int, string> GetKeywords { get; }
+    }
+}
---
+++ WeightenValues.Repository/SQLRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WeightenValues.Repository.Interfaces;
+using WeightenValues.Repository.DTO;
+
+namespace WeightenValues.Repository
+{
+    public class SQLRepository : IRepository
+    {
+        // #todo Would really love to use Dependency Injection instead for this :/
+        private WeightedValuesDataContext _db;
+        public SQLRepository()
+        {
+            _db = new WeightedValuesDataContext();
+        }
+
+        /// <summary>
+        /// Gets all current educations from the database
+        /// </summary>
+        public Dictionary<int, string> GetEducations
+        {
+            get
+            {
+                return _db.Educations.ToDictionary(e => e.ID, e => e.Name);
+            }            
+        }
+
+        /// <summary>
+        /// Gets all current keywords from the database
+        /// </summary>
+        public Dictionary<int, string> GetKeywords
+        {
+            get
+            {
+                return _db.Keywords.ToDictionary(e => e.ID, e => e.Name);
+            }
+        }
+    }
+}
